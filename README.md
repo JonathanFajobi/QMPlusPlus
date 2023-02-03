cd8f7e9f6cfe40d575ae6e591a2a6754a3143fc6
JonathanFajobi <ralufajobi@outlook.com>
2023-02-03 17:25:02 +0000

    Functionality Update v1

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 QMPlusPlus 
 Visit site: 
 
+Site is an ongoing work in progress - functionality is extremely incomplete
+
 Educational web application created to build on and supplement the learning process
 This site contains extra detail regarding courses in the school of electronic engineering to help with course content that can 
 sometimes be confusing and require additional information.
--- scripts/animations.js
+++ scripts/animations.js
@@ -1,21 +1,15 @@
-// const cards = document.getElementsByClassName("box-fit")
 const cards = document.querySelectorAll(".box-fit")
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting)
-        //only peform animation in 
+        //only peform animation in once
         if (entry.isIntersecting) observer.unobserve(entry.target)
     })
 }, {
     threshold: 0.5,
-    // rootMargin: "-100px",
 })
 
 
 cards.forEach(card => {
     observer.observe(card)
 })
-
-
-
-//three.js 
