c2ebdaf000fdc25e29ca6bb1184da681dfcffcfa
JonathanFajobi <ralufajobi@outlook.com>
2023-02-03 17:11:35 +0000

    Observer Update

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,13 +2,13 @@
 const cards = document.querySelectorAll(".box-fit")
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle("show", entry.isIntersecting || entry.isVisible)
+        entry.target.classList.toggle("show", entry.isIntersecting)
         //only peform animation in 
         if (entry.isIntersecting) observer.unobserve(entry.target)
     })
 }, {
-    threshold: 1,
-    rootMargin: "-100px",
+    threshold: 0.5,
+    // rootMargin: "-100px",
 })
 
 
