711532756e26b49decdc35a6f1baf600d5b8c996
JonathanFajobi <ralufajobi@outlook.com>
2023-02-28 22:26:46 +0000

    Preload Func + Worklets Example Update

--- scripts/animations.js
+++ scripts/animations.js
@@ -9,7 +9,20 @@ const observer = new IntersectionObserver(entries => {
     threshold: 0.5,
 })
 
-
 cards.forEach(card => {
     observer.observe(card)
 })
+
+//images 
+const preloadImage = src => 
+  new Promise((resolve, reject) => {
+    const image = new Image()
+    image.onload = resolve
+    image.onerror = reject
+    image.src = src
+  }).then(() => {
+    preloadImage('./assets/anastasiya.png')
+    preloadImage('./assets/armin.png')
+    preloadImage('./assets/blogbg.png')
+    Promise.all(images.map(x => preloadImage(x.src)))
+  })
